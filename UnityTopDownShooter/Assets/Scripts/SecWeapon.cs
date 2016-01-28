685d2fe36a032aaae395a307cead46b77d1c9a41
Daniel Møgelvang Hansen <danielmhansen97@gmail.com>
2016-01-28 15:16:35 +0100

    Added SecondaryWeaponScript and Bulletscript

---
+++ UnityTopDownShooter/Assets/Scripts/SecWeapon.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SecWeapon : MonoBehaviour 
+{
+    public float Damage;
+    public float BulletSpeed;
+
+	void Start () 
+    {
+	
+	}
+	
+	void Update () 
+    {
+	
+	}
+}
