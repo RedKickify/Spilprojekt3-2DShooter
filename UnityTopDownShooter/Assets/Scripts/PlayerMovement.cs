ac221d05f86072e5c47fb5b424269a0690728c38
Daniel Møgelvang Hansen <danielmhansen97@gmail.com>
2016-01-28 14:53:59 +0100

    Added PlayerMovement and PlayerRotation

---
+++ UnityTopDownShooter/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour 
+{
+    private float playerSpeed = 50;
+    private float maxSpeed = 10;
+    private Rigidbody2D rigid;
+
+	void Start () 
+    {
+        rigid = GetComponent<Rigidbody2D>();
+	}
+
+	void Update () 
+    {
+	
+	}
+
+    void FixedUpdate()
+    {
+        PlayerMovementControl();
+    }
+
+    void PlayerMovementControl()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigid.AddRelativeForce(new Vector2(-playerSpeed, 0));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rigid.AddRelativeForce(new Vector2(playerSpeed, 0));
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            rigid.AddRelativeForce(new Vector2(0, playerSpeed));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rigid.AddRelativeForce(new Vector2(0, -playerSpeed));
+        }
+
+        if (rigid.velocity.magnitude > maxSpeed)
+        {
+            rigid.velocity = rigid.velocity.normalized * maxSpeed;
+        }
+    }
+}
---
+++ UnityTopDownShooter/Assets/Scripts/PlayerRotation.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerRotation : MonoBehaviour 
+{
+    private Vector3 playerCamPos;
+    private Vector3 dir;
+
+    private Rigidbody2D rigid;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        PlayerLookAtMouse();
+    }
+
+    void PlayerLookAtMouse()
+    {
+        playerCamPos = Camera.main.WorldToScreenPoint(transform.position);
+        dir = Input.mousePosition - playerCamPos;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90));
+    }
+}
